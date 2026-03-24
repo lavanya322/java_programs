d58e28deb423a2ea1c907d0583a9a1fc38abce4e
Lavanya Naga sri <lavanyanagasri6@gmail.com>
2026-03-24 09:46:35 +0530

    Add files via upload

---
+++ SortArray.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class SortArray {
+public static void main(String[] args) {
+  int[] arr={5,2,8,1,3};
+
+Arrays.sort(arr); // sorting array
+
+System.out.println("Sorted array:");
+for (int num : arr) {
+  System.out.print(num + " ");
+  }
+ }
+}
